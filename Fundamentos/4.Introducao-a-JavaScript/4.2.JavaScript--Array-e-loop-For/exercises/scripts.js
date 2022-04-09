f69313a793461090c3e9ba7a03f81b1522c799de
Othavio Patricio <othavio01patricio@gmail.com>
2022-04-09 08:36:48 -0300

    exercise 8

--- Fundamentos/4.Introducao-a-JavaScript/4.2.JavaScript--Array-e-loop-For/exercises/scripts.js
+++ Fundamentos/4.Introducao-a-JavaScript/4.2.JavaScript--Array-e-loop-For/exercises/scripts.js
@@ -1,11 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //exercise 1
+console.log('exercise 1');
 for (let number of numbers) {
     console.log(number);
 }
 
 //exercise 2
+console.log('exercise 2');
 let sum = 0;
 for (let number of numbers) {
     sum += number;
@@ -13,10 +15,12 @@ for (let number of numbers) {
 console.log(sum);
 
 //exercise 3
+console.log('exercise 3');
 let average = sum / numbers.length;
 console.log(average);
 
 //exercise 4
+console.log('exercise 4');
 if (average > 20) {
     console.log('value bigger than 20');
 }
@@ -25,6 +29,7 @@ else {
 }
 
 //exercise 5
+console.log('exercise 5');
 let bigger = numbers[0];
 for (let index = 1; index < numbers.length; index += 1) {
     if (numbers[index] > bigger) {
@@ -34,6 +39,7 @@ for (let index = 1; index < numbers.length; index += 1) {
 console.log(bigger);
 
 //exercise 6
+console.log('exercise 6');
 let oddNumber = 0;
 for (let index = 0; index < numbers.length; index += 1) {
     if (numbers[index] % 2 != 0) {
@@ -50,6 +56,7 @@ else {
 }
 
 //exercise 7
+console.log('exercise 7');
 let lower = numbers[0];
 for (let index = 1; index < numbers.length; index += 1) {
     if (numbers[index] < lower) {
@@ -57,3 +64,12 @@ for (let index = 1; index < numbers.length; index += 1) {
     }
 }
 console.log(lower);
+
+//exercise 8
+console.log('exercise 8');
+let array = [];
+
+for (let index = 0; index < 25; index += 1) {
+    array.push(index + 1);
+    console.log(array[index]);
+}
