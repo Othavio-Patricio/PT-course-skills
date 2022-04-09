1f29feb1a3fff3359f15a02ebc16bae93abd203b
Othavio Patricio <othavio01patricio@gmail.com>
2022-04-09 08:13:37 -0300

    exercise 4

--- Fundamentos/4.Introducao-a-JavaScript/4.2.JavaScript--Array-e-loop-For/exercises/scripts.js
+++ Fundamentos/4.Introducao-a-JavaScript/4.2.JavaScript--Array-e-loop-For/exercises/scripts.js
@@ -1,13 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //exercise 1
-for(let number of numbers) {
+for (let number of numbers) {
     console.log(number);
 }
 
 //exercise 2
 let sum = 0;
-for(let number of numbers) {
+for (let number of numbers) {
     sum += number;
 }
 console.log(sum);
@@ -15,3 +15,11 @@ console.log(sum);
 //exercise 3
 let average = sum / numbers.length;
 console.log(average);
+
+//exercise 4
+if (average > 20) {
+    console.log('value bigger than 20');
+}
+else {
+    console.log('value lower than 20');
+}
