342f6c3437ecf4909eb5547ff4681baf960572d1
Othavio Patricio <othavio01patricio@gmail.com>
2022-04-09 08:06:02 -0300

    exercise 2

--- Fundamentos/4.Introducao-a-JavaScript/4.2.JavaScript--Array-e-loop-For/exercises/scripts.js
+++ Fundamentos/4.Introducao-a-JavaScript/4.2.JavaScript--Array-e-loop-For/exercises/scripts.js
@@ -1,5 +1,13 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+//exercise 1
 for(let number of numbers) {
     console.log(number);
 }
+
+//exercise 2
+let count = 0;
+for(let number of numbers) {
+    count += number;
+}
+console.log(count);
