d0f49d65e86702dd57cbcccfd5f8e7bff3d80ded
Othavio Patricio <othavio01patricio@gmail.com>
2022-05-11 13:58:25 -0300

    full

---
+++ Fundamentos/6.HTML-e-CSS:-Forms-Flexbox-e-Responsivo/6.2.Bibliotecas-JavaScript-e-Frameworks-CSS/script.js
@@ -0,0 +1,6 @@
+const sendButton = document.getElementById('send-button');
+const deleteButton = document.getElementById('delete-button');
+
+sendButton.addEventListener('click', (event) => {
+  event.preventDefault();
+});
