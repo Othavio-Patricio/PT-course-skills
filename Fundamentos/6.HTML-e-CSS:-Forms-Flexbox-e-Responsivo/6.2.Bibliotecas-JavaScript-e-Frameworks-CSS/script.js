02b8f1d552f4c6f5a05d0edf6a25280f82a5850d
Othavio Patricio <othavio01patricio@gmail.com>
2022-05-11 15:22:28 -0300

    new calendar

--- Fundamentos/6.HTML-e-CSS:-Forms-Flexbox-e-Responsivo/6.2.Bibliotecas-JavaScript-e-Frameworks-CSS/script.js
+++ Fundamentos/6.HTML-e-CSS:-Forms-Flexbox-e-Responsivo/6.2.Bibliotecas-JavaScript-e-Frameworks-CSS/script.js
@@ -4,3 +4,5 @@ const deleteButton = document.getElementById('delete-button');
 sendButton.addEventListener('click', (event) => {
   event.preventDefault();
 });
+
+document.getElementById('data').DatePickerX.init();
